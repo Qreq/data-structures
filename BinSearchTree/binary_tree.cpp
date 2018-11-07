8b293b217526a0811be36354b08650ce03608735
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-07 14:37:27 +0000

    ~ possibly resolved empty LEFT,RIGHT - Seg fault error now present

--- BinSearchTree/binary_tree.cpp
+++ BinSearchTree/binary_tree.cpp
@@ -11,27 +11,30 @@ BinTreeNode* insert_node(BinTreeNode* tree,string node_to_insert){
 		tree = new BinTreeNode(node_to_insert);
 		//cout << "ROOT NODE " << tree->value << endl;
 	}
-	else if (tree->value < node_to_insert){
-		if (tree->left == NULL){
-			tree->left = new BinTreeNode(node_to_insert);
-		}
-		else{
-			insert_node(tree->left,node_to_insert);
-		}
-	}
 	else{
-		if (tree->right == NULL){
-			tree->right = new BinTreeNode(node_to_insert);
+		if (tree->value > node_to_insert){
+			if (tree->left == NULL){
+				tree->left = new BinTreeNode(node_to_insert);
+			}
+			else{
+				tree = insert_node(tree->left,node_to_insert);
+			}
 		}
 		else{
-			insert_node(tree->right,node_to_insert);
+			if (tree->right == NULL){
+				tree->right = new BinTreeNode(node_to_insert);
+			}
+			else{
+				tree = insert_node(tree->right,node_to_insert);
+			}
 		}
 	}
 	return tree;
 }
 
 void pre_order(BinTreeNode* tree){
-	cout << tree->value << ",";
+	cout << "Before Print" << endl;
+	cout << tree->value << " ";
 	if (tree->left != NULL) pre_order(tree->left);
 	if (tree->right != NULL) pre_order(tree->right);
 }
@@ -44,7 +47,7 @@ int main(){
 	BinTreeNode* binarytree = NULL;
 	for (int i = 0; i < words.size();i++){
 		string word = words[i];
-		binarytree = insert_node(binarytree,word);
+		insert_node(binarytree,word);
 	}
 	cout << "Printing Preorder ..." << endl;
 	pre_order(binarytree);
