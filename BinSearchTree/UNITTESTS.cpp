674e186c0216144f6f862df86c04d09afa20fced
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-08 13:10:39 +0000

    + pre_order tests setup and ready for documentation - a.out error bad value when compiling

--- BinSearchTree/UNITTESTS.cpp
+++ BinSearchTree/UNITTESTS.cpp
@@ -2,6 +2,7 @@
 #include "binary_tree.cpp"
 
 int i;
+std::vector<std::string> correct_preorder = {};
 std::vector<std::string> wordset1 = {"This", "is", "the","first","stage"};
 BinTreeNode* binarytree1 = insert_node(NULL,wordset1[0]);
 std::vector<std::string> wordset2 = {"I","really","do","enjoy","Computer", "Science","at","Coventry","University"};
@@ -10,33 +11,36 @@ std::vector<std::string> wordset3 = {"Unit","Tests","are","extremely", "importan
 BinTreeNode* binarytree3 = insert_node(NULL,wordset3[0]);
 
 
-TEST_CASE("Binary Tree is created from words in a vector",["binary tree"]){
+TEST_CASE("Binary Tree is created from words in a vector"){
 	
 }
 
-TEST_CASE("Nodes are printed in pre order","[pre order]"){
+TEST_CASE("Nodes are printed in pre order"){
 	//creates Binary Tree for wordset1
 	for (i = 1; i < wordset1.size();i++){
 		std::string word = wordset1[i];
 		insert_node(binarytree1,word);
 	}
-	std::vector<std::string> preorder_list = {};
-	preorder_list = pre_order(binarytree1,preorder_list);
-	REQUIRE(preorder_list == {"This","is","first","the","stage"});
+	std::vector<std::string> preorder_result = {};
+	preorder_result = pre_order(binarytree1,preorder_result);
+	correct_preorder = {"This","is","first","the","stage"};
+	REQUIRE(preorder_result == correct_preorder);
 	
 	//creates Binary Tree for wordset2
 	for (i = 1; i < wordset2.size();i++){
 		std::string word = wordset2[i];
 		insert_node(binarytree2,word);
 	}
-	preorder_list = pre_order(binarytree2,preorder_list);
-	REQUIRE(preorder_list == {"I","do","Computer","at","Coventry","enjoy","really","Science","University"})
+	preorder_result = pre_order(binarytree2,preorder_result);
+	correct_preorder = {"I","do","Computer","at","Coventry","enjoy","really","Science","University"};
+	REQUIRE(preorder_result == correct_preorder);
 	
 	//creates Binary Tree for wordset3
 	for (i = 1; i < wordset3.size();i++){
 		std::string word = wordset3[i];
 		insert_node(binarytree3,word);
 	}
-	preorder_list = pre_order(binarytree2,preorder_list);
-	REQUIRE(preorder_list == {"Unit","are","extremely","code","important","Tests","when","writing"});
+	preorder_result = pre_order(binarytree2,preorder_result);
+	correct_preorder = {"Unit","are","extremely","code","important","Tests","when","writing"};
+	REQUIRE(preorder_result == correct_preorder);
 }
