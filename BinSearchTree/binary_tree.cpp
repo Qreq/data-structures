d2386adf7a01dab19f1bce519dcc0a9de9b5ffaf
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-10 18:06:32 +0000

    ~Removed commented out main function

--- BinSearchTree/binary_tree.cpp
+++ BinSearchTree/binary_tree.cpp
@@ -91,24 +91,3 @@ vector<string> split_strings(vector<string> lines){
 	}
 	return result;
 }
-
-/*
-int main(){
-	vector<string> wordset = {"This", "is", "the","first","stage","the"};
-	BinTreeNode* binarytree = insert_node(0,wordset[0]);
-	for (int i = 1; i < wordset.size();i++){
-		string word = wordset[i];
-		insert_node(binarytree,word);
-	}
-	cout << "Printing Preorder ..." << endl;
-	vector<string> preorder_list ={};
-	preorder_list = pre_order(binarytree,preorder_list);
-	for (int i = 0; i < preorder_list.size();i++){
-		string node = preorder_list[i];
-		cout << node << " ";
-	}
-	cout << endl;
-	//cout << "Root Node is" << binarytree->value << endl;
-	return 0;
-}
-*/
