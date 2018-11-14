91c554f3a9ecb459506bfbc2fc4325564cbb35ef
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-14 02:08:52 +0000

    Removed vector only method due to it being 'repeated code'

--- BinSearchTree/main.cpp
+++ BinSearchTree/main.cpp
@@ -7,13 +7,18 @@
  * Found here on Solution 2: https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
  * 
  */
-void binary_tree_using_vector(){
-	vector<string> wordset = {"This", "is", "the","first","stage","the"};
-	BinTreeNode* binarytree = insert_node(0,wordset[0]);
-	for (int i = 1; i < wordset.size();i++){
-		string word = wordset[i];
-		insert_node(binarytree,word);
+
+void binary_tree_using_textfile(){
+	//creates binary tree from textfile
+	vector<string> lines,convert_to_tree;
+	lines = read_from_file("filetoread.txt");
+	convert_to_tree = split_strings(lines);
+	BinTreeNode* binarytree = insert_node(0,convert_to_tree[0],NULL);
+	for (int i = 1; i < convert_to_tree.size();i++){
+		string word = convert_to_tree[i];
+		insert_node(binarytree,word,binarytree->parent);
 	}
+	//print pre order of binary tree
 	cout << "Printing Preorder ..." << endl;
 	vector<string> preorder_list ={};
 	preorder_list = pre_order(binarytree,preorder_list);
@@ -22,19 +27,22 @@ void binary_tree_using_vector(){
 		cout << node << " ";
 	}
 	cout << endl;
-	//cout << "Root Node is" << binarytree->value << endl;
-}
-void binary_tree_using_textfile(){
-	vector<string> lines,binarytree;
-	lines = read_from_file("filetoread.txt");
-	binarytree = split_strings(lines);
-	
+	//searches for values
+	vector<string> searches = {"Japanese","this","create","Doll","cheese"};
+	for (int i = 0; i < searches.size();i++){
+		string node = searches[i];
+		cout << "Commencing Search for: " << node << endl;
+		cout << "-------------------------------------" << endl;
+		bool found = find_node(binarytree,node);
+		if (found) {
+			cout << "Node Found" << endl;
+		}
+		else{
+			cout << "Node Not Found" << endl;
+		}
+	}
 }
 int main(){
-	cout << "USING VECTOR" << endl;
-	cout << "............" << endl;
-	binary_tree_using_vector();
-	cout << "............" << endl;
 	cout << "USING TEXT FILE" << endl;
 	cout << "..............." << endl;
 	binary_tree_using_textfile();
