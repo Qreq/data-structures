7948e380e76fa80f7784e624137afb35c898995b
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-15 11:46:39 +0000

    Removed debugging comment

--- BinSearchTree/binary_tree.cpp
+++ BinSearchTree/binary_tree.cpp
@@ -50,7 +50,6 @@ BinTreeNode* find_node(BinTreeNode* tree, string node_to_find){
 		return tree;
 	}
 	else if (tree->right == nullptr && tree->left == nullptr){
-		cout <<"Reached NULL" << endl;
 		return nullptr;
 	}
 	else if (tree->value > node_to_find){
