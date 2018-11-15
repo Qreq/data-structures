4667a1b24411cf38d7a9b5ccf193e582bdce836b
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-15 15:54:54 +0000

    +Inlcluded function to populate graph with vertices

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,3 +1,19 @@
+#include "unweighted_graph.cpp"
+
+using namespace std;
+Graph* populate_graph(Graph* graph, vector<int> nodes){
+	
+	for (int i = 1; i < nodes.size();i++){
+		int node = nodes[i];
+		add_vertice(node,graph);
+	}
+	return graph;
+}
+
 int main(){
+	vector<int> nodes = {3,5,7,4,6,7,9,1,2,4,6,7};
+	int first_node = nodes[0];
+	Graph* my_graph = new Graph(first_node);
+	my_graph = populate_graph(my_graph,nodes);
 	return 0;
 }
