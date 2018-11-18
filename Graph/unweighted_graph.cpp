15b1854fcc72a16f12159986dd7465ed13daeb57
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-18 16:23:30 +0000

    +Adds edges between entered nodes

--- Graph/unweighted_graph.cpp
+++ Graph/unweighted_graph.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+
 #include "Graph.hpp"
+
+using namespace std;
 
 void add_vertice(int new_vertice, Graph* graph_to_add){
 	graph_to_add->vertices.insert(new_vertice);
@@ -9,6 +12,6 @@ void add_edge(Graph* graph_to_add, array<int,2> new_edge){
 	//adds inserted edge to graph on two nodes
 	graph_to_add->edges.insert(new_edge);
 	//connects the vertices to each other, instead of just directionally
-	array<int,2> reverse_edge = (new_edge[1],new_edge[0]);
+	array<int,2> reverse_edge = {new_edge[1],new_edge[0]};
 	graph_to_add->edges.insert(reverse_edge);
 }
