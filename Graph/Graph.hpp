63a9192745559d27cc8070ae0d508f61b2605738
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-18 16:35:50 +0000

    +Adjacency Matrix property is now two-dimensional

--- Graph/Graph.hpp
+++ Graph/Graph.hpp
@@ -3,11 +3,17 @@
 #include <set>
 #include <vector>
 #include <array>
+/*
+ * 
+ * REFERENCE: two-dimensional vector creation by  ropez - http://www.cplusplus.com/forum/general/833/#msg2828
+ * 
+ * 
+ */ 
 class Graph{
 	public:
 	std::set<int> vertices;
 	std::set<std::array<int,2>> edges;
-	std::vector<bool> adj_matrix;
+	std::vector<std::vector<bool>> adj_matrix;
 	Graph(int);
 };
 #endif
