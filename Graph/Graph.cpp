e0951c9f9729cf0b165a7fed1cfedb5f8ca6b730
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-21 17:23:15 +0000

    +Initialises pointer of vector properly now

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,6 +1,8 @@
 #include "Graph.hpp"
+#include<iostream>
+
 Graph::Graph(int vertice){
 	this->vertices.insert(vertice);
 	this->edges = {};
-	this->adj_matrix = {};
+	this->adj_matrix = new std::vector<std::vector<bool>>;
 }
