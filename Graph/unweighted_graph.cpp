fd7fcd984135fb39e7dbfd2beb9983cb39feaa1d
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-21 16:32:38 +0000

    no more syntax errors in populate matrix function, but still seg fault error

--- Graph/unweighted_graph.cpp
+++ Graph/unweighted_graph.cpp
@@ -15,13 +15,26 @@ void add_edge(Graph* graph_to_add, array<int,2> new_edge){
 	array<int,2> reverse_edge = {new_edge[1],new_edge[0]};
 	graph_to_add->edges.emplace(reverse_edge);
 }
-
+Graph* empty_matrix(Graph* graph){
+	for (int i = 0; i < graph->vertices.size(); i++){
+		vector<bool> connections;
+		for (int j = 0; j < graph->vertices.size(); j++){
+			graph->adj_matrix->at(i).at(j) = false;
+			//connections.push_back(false);
+		}
+		cout << "Pushing" << endl;
+		//graph->adj_matrix->at(i).push_back(connections);
+	}
+	cout << "Retuning" << endl;
+	return graph;
+}
 Graph* populate_adjacency_matrix(Graph* graph_with_matrix){
 	set<array<int,2>>::iterator edge_iter;
 	set<int>::iterator vert_iter;
+	
+	graph_with_matrix = empty_matrix(graph_with_matrix);
 	//for use with positions in adjacency matrix vector of graph
 	int x = 0;
-	int	y = 0;
 	
 	/* checks if a vertice is connected with another vertice
 	 * increments x and y for each pass through the edges.
@@ -30,21 +43,17 @@ Graph* populate_adjacency_matrix(Graph* graph_with_matrix){
 	 */
 	for (vert_iter = graph_with_matrix->vertices.begin(); vert_iter != graph_with_matrix->vertices.end(); vert_iter++){
 		cout << "x = " << x << endl;
-		y = 0;
 		vector<bool> vertices_connected = {};
 		for (edge_iter = graph_with_matrix->edges.begin(); edge_iter != graph_with_matrix->edges.end(); edge_iter++){
 			array<int,2> current_edge = *edge_iter;
 			if (*vert_iter == current_edge[0]){
 				cout << "Are Connected" << endl;
-				vertices_connected.emplace_back(true);
-				//graph_with_matrix->adj_matrix->at(x).push_back(true);
+				graph_with_matrix->adj_matrix->at(x).at(current_edge[1]) = true;
 			}
 			else{
 				cout << "not connected" << endl;
-				vertices_connected.emplace_back(false);
-				//graph_with_matrix->adj_matrix->at(x).push_back(false);
 			}
-			y++;
+			
 		}
 		graph_with_matrix->adj_matrix->push_back(vertices_connected);
 		x++;
