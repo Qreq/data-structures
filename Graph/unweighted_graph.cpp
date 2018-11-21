82e3559197901fff904ce932de813cbf5aee3023
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-11-21 11:49:03 +0000

    +Now adds an edge to a list

--- Graph/unweighted_graph.cpp
+++ Graph/unweighted_graph.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include "Graph.hpp"
-
-/*
- * REFERENCE: inserting an array into a set code found here: https://codeyarns.com/2010/07/16/c-stl-inserting-vector-into-set/
-	 * written by: Ashwin - July 26 2010
- * 
- */ 
+ 
 using namespace std;
 
 void add_vertice(int new_vertice, Graph* graph_to_add){
@@ -15,11 +10,12 @@ void add_vertice(int new_vertice, Graph* graph_to_add){
 
 void add_edge(Graph* graph_to_add, array<int,2> new_edge){
 	//adds inserted edge to graph on two nodes
-	copy(new_edge.begin(),new_edge.end(),inserter(graph_to_add->edges,graph_to_add->edges.end()));
-	graph_to_add->edges.insert(new_edge);
+	cout << "before first emplace" << endl;
+	graph_to_add->edges.emplace(new_edge);
+	cout << "after furst emplace" << endl;
 	//connects the vertices to each other, instead of just directionally
 	array<int,2> reverse_edge = {new_edge[1],new_edge[0]};
-	copy(reverse_edge.begin(),reverse_edge.end(),inserter(graph_to_add->edges,graph_to_add->edges.end()));
+	graph_to_add->edges.emplace(reverse_edge);
 }
 
 Graph* populate_adjacency_matrix(Graph* graph_with_matrix){
